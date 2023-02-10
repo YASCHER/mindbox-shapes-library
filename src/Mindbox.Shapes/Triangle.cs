13daf5044eced85e5f2404705cc6c5d44835fe02
Aleksandr Goncharov <cooperjkr@ya.ru>
2023-02-10 09:38:21 +0300

    triangle: добавил класс Triangle

---
+++ src/Mindbox.Shapes/Triangle.cs
@@ -0,0 +1,26 @@
+﻿namespace Mindbox.Shapes;
+
+public class Triangle : Shape
+{
+    public double A { get; }
+
+    public double B { get; }
+
+    public double C { get; }
+
+    public Triangle(double a, double b, double c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public override double CalculateArea()
+    {
+        var p = CalculatePerimeter() / 2;
+
+        return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+    }
+
+    public override double CalculatePerimeter() => A + B + C;
+}
