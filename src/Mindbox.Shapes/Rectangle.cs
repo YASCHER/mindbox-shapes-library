a3c1293a6d3a6894c45139fc203bcf206531acd5
Aleksandr Goncharov <cooperjkr@ya.ru>
2023-02-10 14:17:35 +0300

    rectangle: добавил Rectangle

---
+++ src/Mindbox.Shapes/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿namespace Mindbox.Shapes;
+
+public class Rectangle : Shape
+{
+    public double A { get; }
+
+    public double B { get; }
+
+    public Rectangle(double a, double b)
+    {
+        if (a <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(a));
+        }
+        else if (b <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(b));
+        }
+
+        A = a;
+        B = b;
+    }
+
+    public override double CalculateArea() => A * B;
+
+    public override double CalculatePerimeter() => 2 * A + 2 * B;
+}
